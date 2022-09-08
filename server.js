1cf6f1867978c9e5d575c58e8ccfa5238b37386e
angelica <angelica.cavero@gmail.com>
2022-09-09 00:20:42 +0200

    server port from env variable

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = process.env.SERVER_PORT || 3001
 const fs = require('fs')
 
 const phones = JSON.parse(fs.readFileSync('phones.json'))
